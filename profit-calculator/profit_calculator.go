c113e5dafbddc74d82d340164ad45bb5d3b1bb96
paalomnikdev <paalomnik@gmail.com>
2024-09-04 17:06:24 +0300

    validation + data storage added

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"fmt"
+	"errors"
+	"os"
+	"time"
 )
 
 func main() {
 	var revenue, expenses, taxRate float64
+	var err error
 
 	prompt("Revenue: ", &revenue)
+	err = validateInput(revenue)
+	if err != nil {
+		renderError(err)
+		return
+	}
 	prompt("Expenses: ", &expenses)
+	err = validateInput(expenses)
+	if err != nil {
+		renderError(err)
+		return
+	}
 	prompt("Tax Rate: ", &taxRate)
+	err = validateInput((taxRate))
+	if err != nil {
+		renderError(err)
+		return
+	}
 
 	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
 
-	fmt.Printf("Earnings Before Tax: %.2f \n", ebt)
-	fmt.Printf("Profit: %.2f \n", profit)
-	fmt.Printf("Ratio: %.2f \n", ratio)
+	result := fmt.Sprintf(`Earnings Before Tax: %.2f
+Profit: %.2f
+Ratio: %.2f
+`, ebt, profit, ratio)
+
+	fmt.Print(result)
+	err = storeCalculationResult(revenue, expenses, taxRate, result)
+	if err != nil {
+		fmt.Println("_____ERROR_____")
+		panic(err)
+	}
 }
 
 func prompt(requestText string, val *float64) {
@@ -29,4 +56,29 @@ func calculate(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ratio := ebt/profit
 
 	return ebt, profit, ratio
+}
+
+func validateInput(input float64) error {
+	if input <= 0 {
+		return errors.New("provided value should b greater than zero")
+	}
+
+	return nil
+}
+
+func renderError(errorText error) {
+	fmt.Println("-----ERROR-----")
+	fmt.Println(errorText)
+	fmt.Println("-----ERROR-----")
+}
+
+func storeCalculationResult(revenue float64, expenses float64, taxRate float64, result string) error {
+	dataToStore := fmt.Sprintf(`Revenue: %.2f Expenses: %.2f Tax Rate: %.2f
+%s`, revenue, expenses, taxRate, result)
+	timestamp := time.Now().Format("20060102_150405")
+	filename := fmt.Sprintf("calculation_result_%s.txt", timestamp)
+
+	err := os.WriteFile(filename, []byte(dataToStore), 0644)
+
+	return err
 }
