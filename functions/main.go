75e2025b7b095705e1d6d9dc5dc9d1117ebcd13a
paalomnikdev <paalomnik@gmail.com>
2024-09-13 12:15:50 +0300

    functions deep dive

---
+++ functions/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	doubled := transformNumbers(&numbers, getTransformerFunction("double"))
+
+	fmt.Println(doubled)
+
+	fmt.Println(
+		transformNumbers(&numbers, getTransformerFunction("triple")),
+	)
+
+	fmt.Println(
+		transformNumbers(&numbers, getTransformerFunction("whatever")),
+	)
+
+	fmt.Println(
+		transformNumbers(&numbers, createTransformer(5)),
+	)
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	tNumbers := []int{}
+
+	for _, v := range *numbers {
+		tNumbers = append(tNumbers, transform(v))
+	}
+
+	return tNumbers
+}
+
+func getTransformerFunction(transformationType string) transformFn {
+	switch transformationType {
+	case "double":
+		return double
+	case "triple":
+		return triple
+	default:
+		return func(number int) int {
+			return number
+		}
+	}
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
+
+func createTransformer(factor int) transformFn {
+	return func(number int) int {
+		return number * factor
+	}
+}
