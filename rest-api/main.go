c5ae42bf83263cab2e4ea386cfcab4c3deee3f0d
paalomnikdev <paalomnik@gmail.com>
2024-09-23 12:53:42 +0300

    rest api initial

---
+++ rest-api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(ctx *gin.Context) {
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Can't parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created.", "event": event})
+}
